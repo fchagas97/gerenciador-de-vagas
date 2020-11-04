fddc3b9ca7fb854c04f18a7b095697cb8dccf7ce
Francisco das Chagas <14143443+fchagas97@users.noreply.github.com>
2020-11-03 23:12:33 -0300

    Update layout.php

--- estacionamento/views/layout.php
+++ estacionamento/views/layout.php
@@ -107,7 +107,7 @@
                 </div>
             </div>
         </div>
-        <script src="static/js/jquery-3.4.1.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
         <script src="static/js/bootstrap-4.3.1.bundle.min.js"></script>
 
         <script type="text/javascript">
